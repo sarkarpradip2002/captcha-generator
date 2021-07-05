55948aeab631b48dc444354511786dfb898f67ab
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2021-07-05 11:39:33 +0530

    rendered image dynamically

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 captchaString=null;
+const imageSrc = "https://github.com/sudip-mondal-2002/captcha-generator/blob/master/images/"
+const imageQuery = "?raw=true"
 const generatecaptcha = ()=>{
     captchaString = ""
     for(let i=1; i<=5; i++){
         randomLetter = String.fromCharCode(Math.floor(Math.random()*26)+65);
         imageId = "#"+i;
         captchaString += randomLetter;
-        document.getElementById(imageId).setAttribute('src',"images/"+randomLetter+".jpg");
+        document.getElementById(imageId).setAttribute('src',imageSrc+randomLetter+".jpg"+imageQuery);
         document.getElementById("userInput").classList.remove('hide');
         document.getElementById("checkButton").classList.remove('hide');
     }
